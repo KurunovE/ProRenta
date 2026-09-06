ffa259218a92b665755ba6ef2840f9dbc8fb24a1
Egor Kurunov <eegor2257@gmail.com>
2026-09-06 16:38:00 +0300

    Добавил метод в контроллере для получения списка курсов валют

--- src/main/java/com/prorenta/financeservice/controller/impl/CurrencyControllerImpl.java
+++ src/main/java/com/prorenta/financeservice/controller/impl/CurrencyControllerImpl.java
@@ -3,12 +3,15 @@ package com.prorenta.financeservice.controller.impl;
 import com.prorenta.financeservice.controller.CurrencyController;
 import com.prorenta.financeservice.model.dto.CurrencyResponseDto;
 import com.prorenta.financeservice.model.dto.ListCurrenciesResponseDto;
+import com.prorenta.financeservice.model.dto.ListCurrencyRatesResponseDto;
+import com.prorenta.financeservice.service.CurrencyRateService;
 import com.prorenta.financeservice.service.CurrencyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Slf4j
@@ -17,6 +20,7 @@ import java.util.UUID;
 public class CurrencyControllerImpl implements CurrencyController {
 
     private final CurrencyService currencyService;
+    private final CurrencyRateService currencyRateService;
 
     @Override
     public ResponseEntity<ListCurrenciesResponseDto> getCurrencies() {
@@ -30,5 +34,21 @@ public class CurrencyControllerImpl implements CurrencyController {
         log.debug("Запрос на получение валюты: currencyId={}", currencyId);
         CurrencyResponseDto currencyResponseDto = currencyService.getCurrency(currencyId);
         return ResponseEntity.ok(currencyResponseDto);
+    }
+
+    @Override
+    public ResponseEntity<ListCurrencyRatesResponseDto> getCurrencyRates(
+            String currencyCode,
+            LocalDate startDate,
+            LocalDate endDate
+    ) {
+        log.debug("Запрос на получение курсов валюты {}: startDate={}, endDate={}",
+                currencyCode, startDate, endDate);
+        ListCurrencyRatesResponseDto listCurrencyRatesResponseDto = currencyRateService.getCurrencyRates(
+                currencyCode,
+                startDate,
+                endDate
+        );
+        return ResponseEntity.ok(listCurrencyRatesResponseDto);
     }
 }
